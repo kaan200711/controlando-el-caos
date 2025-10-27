9aad4b967e8b33c6b82100d875a6e1cc083d4b14
Marcel <marcel.codiperpi@alumnes.ilerna.com>
2025-10-27 20:04:34 +0100

    Revert "Reapply "potencias""
    This reverts commit 53c4ede52f69496b9303a1d321129688cd435c6d.

--- control_codigo1.java
+++ control_codigo1.java
@@ -12,9 +12,6 @@ public class control_codigo1 {
         System.out.println("Resta: " + (a - b));
         System.out.println("Multiplicación: " + (a * b));
         System.out.println("División: " + (a / b));
-        
-        int potencia = (int) Math.pow(a, b);
-        System.out.println("Potencia: " + a + " ^ " + b + " = " + potencia);
 
 	}
 
