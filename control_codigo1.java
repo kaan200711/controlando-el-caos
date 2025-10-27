712f939e44e5beef905d438cee6f8a5f5cf29418
Kaan Hyusein Asan <kaanhyusein31@alumnes.ilerna.com>
2025-10-27 19:53:43 +0100

    Codigo_base
    Este es el código sobre el que trabajará cada integrante del equipo

---
+++ control_codigo1.java
@@ -0,0 +1,18 @@
+package control_del_caos;
+
+public class control_codigo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    System.out.println("=== Calculadora Básica ===");
+        int a = 10;
+        int b = 5;
+ 
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicación: " + (a * b));
+        System.out.println("División: " + (a / b));
+
+	}
+
+}
